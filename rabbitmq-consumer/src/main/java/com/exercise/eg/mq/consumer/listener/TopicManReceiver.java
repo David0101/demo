10f5cb66b461077ad7ef572da2a61fba81556618
jim <ljw5332545>
2020-05-14 16:18:39 +0800

    TopicManReceiver

---
+++ rabbitmq-consumer/src/main/java/com/exercise/eg/mq/consumer/listener/TopicManReceiver.java
@@ -0,0 +1,23 @@
+package com.exercise.eg.mq.consumer.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @program: demo
+ * @author: jim
+ * @create: 2020-05-14 16:11
+ */
+
+@Component
+@RabbitListener(queues = "topic.man")
+public class TopicManReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("TopicManReceiver消费者收到消息  : " + testMessage.toString());
+    }
+}
---
+++ rabbitmq-consumer/src/main/java/com/exercise/eg/mq/consumer/listener/TopicTotalReceiver.java
@@ -0,0 +1,23 @@
+package com.exercise.eg.mq.consumer.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @program: demo
+ * @author: jim
+ * @create: 2020-05-14 16:12
+ */
+
+@Component
+@RabbitListener(queues = "topic.woman")
+public class TopicTotalReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("TopicTotalReceiver消费者收到消息  : " + testMessage.toString());
+    }
+}
